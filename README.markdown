2e7911e416ed264378fc417f0aca6727cb0baa1b
rita8216 <rita@chenjialundeMacBook-Air.local>
2021-06-27 19:44:02 +0800

    upload

---
+++ README.markdown
@@ -0,0 +1,5 @@
+# 
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/chia-lun/pen/vYxoWbv](https://codepen.io/chia-lun/pen/vYxoWbv).
+
+
---
+++ js/script.js
@@ -0,0 +1,255 @@
+var videodatas=[
+  {
+    video_img: "https://i.imgur.com/jnrYhEs.png",
+    time:"17:32",
+    title: "✿沈✿ 台北美食 超讚夜市 巨推 雙城街夜市 多汁唐揚雞 美味芒果燉奶 ",
+    creator: "陪沈團",
+    creator_img: "https://i.imgur.com/zV584BB.jpg",
+    views: "34.9萬",
+    date: "5個月"
+  },
+  {
+    video_img: "https://i.imgur.com/o8sJkCU.png",
+    time:"11:43",
+    title: "開箱15間連鎖速食隱藏甜點！(下) 最強乳酪蛋糕！？一次吃35個甜點！ 我最愛的炸雞店？ 早餐店神級甜點？｜內內",
+    creator: "內內Nene",
+    creator_img: "https://i.imgur.com/aztR4D6.png",
+    views: "4.9萬",
+    date: "1 週"
+  },
+
+  {
+    video_img: "https://i.imgur.com/qWklQd6.png",
+    time:"3:51",
+    title: "曾博恩 Brian Tseng 【妹妹防護隊】feat. 金魚腦Goldfish Brain｜ Official Music Video",
+    creator: "STR Network",
+    creator_img: "https://i.imgur.com/ZkH8FVf.png",
+    views: "69萬",
+    date: "4 週"
+  },
+
+  {
+    video_img: "https://i.imgur.com/zNMKYv6.png",
+    time:"15:13",
+    title: "蔡哥場勘-單人勇闖動物園挑戰你尬的底線 feat.上班不要看女同事",
+    creator: "蔡哥",
+    creator_img: "https://i.imgur.com/XxUXihT.png",
+    views: "16萬",
+    date: "3 個月"
+  },
+    {
+    video_img: "https://i.imgur.com/DrHBxyg.png",
+    time:"7:25",
+    title: "【#狗屎寫手】黑歷史！那些被狗屎寫手刪掉的段子",
+    creator: "STR Network",
+    creator_img: "https://i.imgur.com/ZkH8FVf.png",
+    views: "15萬",
+    date: "1 週"
+  },
+
+  {
+    video_img: "https://i.imgur.com/vr23MtK.png",
+    time:"1:02:09",
+    title: "〔蔡哥直播〕#120 珍惜理所當然",
+    creator: "蔡哥",
+    creator_img: "https://i.imgur.com/XxUXihT.png",
+    views: "2.8萬",
+    date: "3 天"
+  },
+    {
+    video_img: "https://i.imgur.com/lHWhXUw.png",
+    time:"24:37",
+    title: "《下班去吃飯第十四集》那隻魚仔終於游到木曜啦！！ feat. 盧廣仲 Crowd Lu",
+    creator: "木曜4超玩",
+    creator_img: "https://i.imgur.com/QSFm2mv.png",
+    views: "83萬",
+    date: "2 週"
+  },
+  {
+    video_img: "https://i.imgur.com/YZeN01M.png",
+    time:"31:49",
+    title: "西子灣尋獲失散姪女 高分貝掀家庭革命｜羅時豐【DL.不務正YA】第三季｜第9集 feat.中山大學",
+    creator: "羅時豐D.L 不務正YA",
+    creator_img: "https://i.imgur.com/kdZ9obZ.png",
+    views: "50萬",
+    date: "1 個月"
+  },
+
+      {
+    video_img: "https://i.imgur.com/OutVvuZ.png",
+    time:"31:42",
+    title: "【博恩站起來】那些被《雙聲道》刪掉的段子 完整31分鐘 2021 非喜劇特輯 THIS IS NOT A SPECIAL",
+    creator: "STR Network",
+    creator_img: "https://i.imgur.com/ZkH8FVf.png",
+    views: "78萬",
+    date: "3 週"
+  },
+  {
+    video_img: "https://i.imgur.com/42zq20y.png",
+    time:"34:31",
+    title: "世上只有家人好 有爸的孩子像個寶｜羅時豐【DL.不務正YA】｜第三季｜第13集 feat.衛生紙",
+    creator: "羅時豐D.L 不務正YA",
+    creator_img: "https://i.imgur.com/kdZ9obZ.png",
+    views: "30萬",
+    date: "1 週"
+  },
+    {
+    video_img: "https://i.imgur.com/JWr6rzd.png",
+    time:"4:25",
+    title: "告五人 Accusefive [ 與海無關 Beyond The Sea ] Official Music Video",
+    creator: "告五人Accusefive ",
+    creator_img: "https://i.imgur.com/zgRRJ6n.png",
+    views: "111萬",
+    date: "4 個月"
+  },
+{
+    video_img: "https://i.imgur.com/7pvOBqZ.png",
+    time:"15:55",
+    title: "✿沈✿ 吃爆台中 不到24小吃喝台中16家全記錄 台中美食 台中住宿 台中旅館 #沈農氏",
+    creator: "陪沈團",
+    creator_img: "https://i.imgur.com/zV584BB.jpg",
+    views: "43萬",
+    date: "1 年"
+  },
+      {
+    video_img: "https://i.imgur.com/EljcOdn.png",
+    time:"10:17",
+    title: "【馬來西亞沙巴系列ep.5】天然溫泉、瀑布醍醐灌頂！在地大排檔晚餐！",
+    creator: "欸你這週要幹嘛",
+    creator_img: "https://i.imgur.com/yiYyva2.png",
+    views: "36萬",
+    date: "5 天"
+  },
+  {
+    video_img: "https://i.imgur.com/1B5OoAr.png",
+    time:"17:31",
+    title: "✿沈✿ 一日吃爆台南 12家美食馬拉松全記錄 早八開跑 台南美食 推薦 #沈農氏",
+    creator: "陪沈團",
+    creator_img: "https://i.imgur.com/zV584BB.jpg",
+    views: "31萬",
+    date: "1 年"
+  },
+
+  {
+    video_img: "https://i.imgur.com/Ca2lJ2Z.png",
+    time:"22:39",
+    title: "吃爆台中36間美食！(下) 鳳梨夏威夷炒飯！？香菜奶茶？？ 多汁咖哩蛋餅 我最愛的厚鬆餅 日式刨冰｜大胃王內內",
+    creator: "內內Nene",
+    creator_img: "https://i.imgur.com/aztR4D6.png",
+    views: "6.6萬",
+    date: "1 個月"
+  },
+       {
+    video_img: "https://i.imgur.com/l27JA9x.png",
+    time:"29:26",
+    title: "大港開唱｜嘻哈炸翻南霸天 愛無中指造勢趴｜羅時豐【DL.不務正YA】｜第三季｜第8集｜幕後花絮 @告五人Accusefive @美麗本人",
+    creator: "羅時豐D.L 不務正YA",
+    creator_img: "https://i.imgur.com/kdZ9obZ.png",
+    views: "44萬",
+    date: "1 個月"
+  },
+  {
+    video_img: "https://i.imgur.com/0GDb103.png",
+    time:"18:32",
+    title: "《下班去吃飯第十二集》到底～哪個東西才是可以吃的啦？！feat.動力火車",
+    creator: "木曜4超玩",
+    creator_img: "https://i.imgur.com/QSFm2mv.png",
+    views: "258萬",
+    date: "3 個月"
+  },
+
+    {
+    video_img: "https://i.imgur.com/EWauL5C.png",
+    time:"32:40",
+    title: "蔡哥-戒飲料30天全紀錄之超真實日常",
+    creator: "蔡哥",
+    creator_img: "https://i.imgur.com/XxUXihT.png",
+    views: "75萬",
+    date: "2 週"
+  },
+      {
+    video_img: "https://i.imgur.com/E7313LH.png",
+    time:"4:21",
+    title: "告五人 Accusefive [ 醜人多作怪 Mischief ] Official Music Video",
+    creator: "告五人Accusefive ",
+    creator_img: "https://i.imgur.com/zgRRJ6n.png",
+    views: "195萬",
+    date: "4 個月"
+  },
+  {
+    video_img: "https://i.imgur.com/oUdsToQ.png",
+    time:"1:08:02",
+    title: "《一日系列第一百四十九集》歡迎光臨麥當勞！前輩KID帶領菜鳥邰智源炸薯條做漢堡，叭啦叭叭叭～I’m lovin' it McDonald's -一日麥當勞",
+    creator: "木曜4超玩",
+    creator_img: "https://i.imgur.com/QSFm2mv.png",
+    views: "358萬",
+    date: "6 個月"
+  },
+  {
+    video_img: "https://i.imgur.com/Oyk7vY4.png" ,
+    time:"13:21",
+    title: "【露營系列】揭曉大姐肚子裡第二胎的性別啦！苗栗露營！",
+    creator: "欸你這週要幹嘛",
+    creator_img: "https://i.imgur.com/yiYyva2.png",
+    views: "137萬",
+    date: "1 年"
+  },
+    {
+    video_img: "https://i.imgur.com/qUpNlzO.png",
+    time:"24:05",
+    title: "你全家都美食沙漠！新竹22間美食！晚上住在香客大樓 ？新竹必吃肉桂捲 超軟嫩炒鴨血 超夯紅豆餅 賣冰的麵店｜抖內內",
+    creator: "內內Nene",
+    creator_img: "https://i.imgur.com/aztR4D6.png",
+    views: "10萬",
+    date: "1 個月"
+  },
+
+    {
+    video_img: "https://i.imgur.com/d14D1IU.png",
+    time:"2:38",
+    title: "【頻道主題曲】《欸你這週要幹嘛》Official Music Video",
+    creator: "欸你這週要幹嘛",
+    creator_img: "https://i.imgur.com/yiYyva2.png",
+    views: "91萬",
+    date: "1 個月"
+  },
+  {
+    video_img: "https://i.imgur.com/sRaUDj3.png",
+    time:"5:54",
+    title: "告五人 Accusefive [ 在這座城市遺失了你 Where I Lost Us ] Official Music Video（電視劇《她們創業的那些鳥事》插曲）",
+    creator: "告五人Accusefive ",
+    creator_img: "https://i.imgur.com/zgRRJ6n.png",
+    views: "790萬",
+    date: "5 個月"
+  },
+
+
+  
+];
+var btndatas=["全部","音樂","烹飪節目","直撥中","旅遊勝地","手工藝","網頁教學","卡通","發燒排行","最新上傳","懸疑推理","幼童","美食日記","生活","綜藝節目","美容","熱門推薦"];
+
+var vm=new Vue({
+  el: "#youtube",
+  data:{
+    videos: videodatas,
+    btns: btndatas
+  }
+  // mounted: function(){
+   // $.ajax({
+   //    url:"https://awiclass.monoame.com/api/command.php?type=get&name=projects",
+   //    success: function(res){
+   //      vm.works=JSON.parse(res);
+   //    }
+   // });
+});
+
+// if($(window).width<600px){
+  
+// }else if($(window).width<850px){
+  
+// }else if($(window).width<1024px){
+  
+// }else{
+  
+// };
+  
